6a7fdc872b3fcdb411bc6a4777e525d0da525950
lifang <santhe_2004@126.com>
2012-03-21 13:56:28 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniSite::Application.config.secret_token = '0a52d07080d4afc5b11d831b1362de971cba7bf992fbea843c872b68c301ff1fef2549181e68c7bd92b81e5f1a4ed0ca8e4c3f9dca68fc592209728ddf74e0d7'
