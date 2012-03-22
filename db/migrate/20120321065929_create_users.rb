d942ec11a75547520ac798a7c3c16e368e2d3d6d
qianjun <er6788@126.com>
2012-03-22 16:46:42 +0800

    qianjun

--- db/migrate/20120321065929_create_users.rb
+++ db/migrate/20120321065929_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.integer :id
-      t.integer :secret_id
+      t.string :secret_id
     end
     add_index :users, :secret_id
   end
